028f572805085c899039a65eba1b9db3d4f3f199
Paulo Eduardo Pacheco <eduardoguitar@gmail.com>
2018-12-09 19:03:02 +0100

    introduced .db.properties config file for local development

--- src/main/kotlin/com/study/multi/docker/aws/config/DatabaseConfiguration.kt
+++ src/main/kotlin/com/study/multi/docker/aws/config/DatabaseConfiguration.kt
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
+import org.springframework.context.annotation.PropertySource
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
@@ -12,6 +13,7 @@ import org.springframework.data.redis.serializer.GenericToStringSerializer
 
 @Configuration
 @ComponentScan("com.study.multi.docker.aws")
+@PropertySource(value = ["classpath:/config/.db.properties"], ignoreResourceNotFound = true)
 class DatabaseConfiguration {
 
     @Bean
